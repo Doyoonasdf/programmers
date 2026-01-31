6cb9ea477b2e7380296e5ed4eeb652886d3c7dc6
Doyoonasdf <won1hana@naver.com>
2026-01-31 10:19:37 +0900

    공배수

---
+++ src/main/java/CommonMultiple.java
@@ -0,0 +1,12 @@
+//공배수
+public class CommonMultiple {
+    public int solution(int number, int n, int m) {
+        if(number % n == 0 && number % m == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+
+    }
+    
+}
