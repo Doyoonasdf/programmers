31e8b13db6c3562f39857f03465cb30bed6c606c
Doyoonasdf <won1hana@naver.com>
2026-02-10 20:49:05 +0900

    첫번째로 나오는 음수

---
+++ src/main/java/com/example/programers/FirstNagativeNum.java
@@ -0,0 +1,24 @@
+package com.example.programers;
+
+/*
+* 정수 리스트 num_list가 주어질 때, 첫 번째로 나오는 음수의 인덱스를 return하도록 solution 함수를 완성해주세요. 음수가 없다면 -1을 return합니다.
+* */
+
+//첫번째로 나오는 음수
+public class FirstNagativeNum {
+    static int solution(int[] num_list) {
+        int answer = -1;
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] < 0) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{12, 4, 15, 46, 38, -2, 15}));
+    }
+
+}
